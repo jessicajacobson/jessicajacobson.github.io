1ec28c43714d1c2d9bfcb5bd06e3008a5235eebb
MollyK-A <73881134+MollyK-A@users.noreply.github.com>
2023-03-05 11:05:37 +0100

    background image

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { P } from '../ui/Paragraph';
+// import ReactMarkdown from 'react-markdown'
+// import * as ReactDom from 'react-dom';
 
 export const Info: React.VFC = () => (
   <div>
--- src/stitches.config-old.ts
+++ src/stitches.config-old.ts
@@ -1,18 +1,17 @@
 import { createCss, StitchesCss } from '@stitches/react';
-// import img from 'jessicajacobson.github.io/img/bkg.png';
 
 export const stitchesConfig = createCss({
   theme: {
     colors: {
-      pageBackground: 'rgb(240,240,240)', // bakgrundsfärg
+      pageBackground: 'rgb(240,240,240)',
       backgroundContrast: 'rgb(216,216,216)',
-      highContrast: 'rgb(0,0,0)', //text
-      lowContrast: '#c8def4',
-      formElementsBackground: 'rgb(250,250,250)', // inte använd
+      highContrast: 'rgb(0,0,0)',
+      lowContrast: 'rgb(128,128,128)',
+      formElementsBackground: 'rgb(250,250,250)',
       red: 'hsl(0,100%,50%)',
       orange: 'hsl(30,100%,50%)',
       yellow: 'hsl(51,100%,40%)',
-      green: 'hsl(120,100%,33%)', // same as rgb(0,168,0) // understrykning
+      green: 'hsl(120,100%,33%)', // same as rgb(0,168,0)
       blue: 'hsl(240,100%,50%)',
       purple: 'hsl(270,100%,60%)',
     },
@@ -90,9 +89,5 @@ export const globalCss = createGlobalCss({
   '#root': {
     minHeight: '100%',
     backgroundColor: '$pageBackground',
-    backgroundImage: 'url("/img/bkg.png")',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
   },
 });
